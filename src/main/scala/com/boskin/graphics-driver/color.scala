d89c6fe5bef59281eb7e339eb1bba2d83ccd3d71
Boskin Erkocevic <boskin.erkocevic@gmail.com>
2019-04-01 23:38:42 -0600

    Fixed some formatting.

--- src/main/scala/com/boskin/graphics-driver/color.scala
+++ src/main/scala/com/boskin/graphics-driver/color.scala
@@ -9,9 +9,7 @@ case class ColorSpec(rWidth: Int, gWidth: Int, bWidth: Int, alphaWidth: Int) {
   val width: Int = rWidth + gWidth + bWidth + alphaWidth
 }
 
-class Color(val spec: ColorSpec)
-  extends Bundle {
-
+class Color(val spec: ColorSpec) extends Bundle {
   val r = UInt(spec.rWidth.W)
   val g = UInt(spec.gWidth.W)
   val b = UInt(spec.bWidth.W)
