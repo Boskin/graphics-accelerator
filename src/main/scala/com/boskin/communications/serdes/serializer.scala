98a15a6d6d1ed5be64efa73fd14eab82ab5112cd
Boskin Erkocevic <boskin.erkocevic@gmail.com>
2019-05-27 18:12:55 -0600

    Added validOut logic that only sometimes works. Also updated parameter descriptions.

--- src/main/scala/com/boskin/communications/serdes/serializer.scala
+++ src/main/scala/com/boskin/communications/serdes/serializer.scala
@@ -6,6 +6,7 @@ import chisel3.util.log2Ceil
 
 import com.boskin.synchronization.{AsyncFIFO, ShiftRegister}
 
+// IO class
 class SerializerIO(val pktWidth: Int, val outputValid: Boolean) extends Bundle {
   val sClk = Input(Clock())
   val sReset = Input(Bool())
@@ -22,20 +23,50 @@ class SerializerIO(val pktWidth: Int, val outputValid: Boolean) extends Bundle {
   }
 }
 
+// Class that represents a payload structurally
+class Payload(val startSeqWidth: Int, val pktWidth: Int, val stopSeqWidth: Int)
+  extends Bundle {
+
+  val startSeq = if (startSeqWidth > 0) {
+    Vec(startSeqWidth, Bool())
+  } else {
+    null
+  }
+
+  val pktSeq = if (pktWidth > 0) {
+    Vec(pktWidth, Bool())
+  } else {
+    null
+  }
+
+  val stopSeq = if (stopSeqWidth > 0) {
+    Vec(stopSeqWidth, Bool())
+  } else {
+    null
+  }
+}
+
 /* Parameter explanation:
  * pktWidth: width of each packet to be serialized
- * fifoDepth: depth of the packet FIFO before serialization
- * continuous: if true, allows the module to continuously transmit bits with no
- * pause between packets
- * defaultVal: if 1 or 0 is given, the serializer will output this value by
- * default if no valid data is being transmitted, otherwise, a validOut signal
- * is added to indicate valid output */
-class Serializer(pktWidth: Int, continuous: Boolean,
-  defaultVal: Int, fifoDepth: Int) extends Module {
+ *
+ * startSeq: binary sequence to be output before the packet
+ *
+ * stopSeq: binary sequence to be output after the packet
+ *
+ * defaultSOut: hold sOut to this value when not transmitting anything
+ *
+ * outputValid: if true, adds a validOut port, validOut is held high only
+ * during the packet transmission (not during startSeq or stopSeq)
+ *
+ * fifoDepth: depth of the packet FIFO before serialization */
+class Serializer(pktWidth: Int, startSeq: Seq[Bool], stopSeq: Seq[Bool],
+   defaultSOut: Boolean, outputValid: Boolean, fifoDepth: Int) extends Module {
 
-  val counterWidth = log2Ceil(pktWidth)
-  val outputValid = (defaultVal != 0) && (defaultVal != 1)
-  val defaultValBool = defaultVal != 0
+  val startSeqWidth = startSeq.length
+  val stopSeqWidth = stopSeq.length
+  val payloadWidth = pktWidth + startSeqWidth + stopSeqWidth
+  val counterWidth = log2Ceil(payloadWidth)
+
 
   val io = IO(new SerializerIO(pktWidth, outputValid))
 
@@ -51,7 +82,7 @@ class Serializer(pktWidth: Int, continuous: Boolean,
 
 
   withClockAndReset(io.sClk, io.sReset) {
-    val shiftRegInst = Module(new ShiftRegister(Bool(), pktWidth, true, true))
+    val shiftRegInst = Module(new ShiftRegister(Bool(), payloadWidth, true, true))
 
     val shiftCount = RegInit(0.U(counterWidth.W))
     val validOut = if (outputValid) {
@@ -71,55 +102,59 @@ class Serializer(pktWidth: Int, continuous: Boolean,
 
     val shiftEn = RegInit(false.B)
 
-    shiftRegInst.io.din := false.B
+    // Concatenate the sequences and packet to form the payload
+    val payload = Wire(new Payload(startSeqWidth, pktWidth, stopSeqWidth))
+
+    if (startSeqWidth > 0) {
+      payload.startSeq := VecInit(startSeq.reverse)
+    }
+
+    payload.pktSeq := fifoInst.io.rdReq.data.toBools
+
+    if (stopSeqWidth > 0) {
+      payload.stopSeq := VecInit(stopSeq.reverse)
+    }
+
+    // Connect shift register IO
+    shiftRegInst.io.din := defaultSOut.B
     shiftRegInst.io.en := shiftEn
     shiftRegInst.io.ld := fifoInst.io.rdReq.valid
-    shiftRegInst.io.ldVal := fifoInst.io.rdReq.data.toBools
+    shiftRegInst.io.ldVal := VecInit(payload.asUInt.toBools)
 
     fifoInst.io.rdReq.en := !fifoInst.io.rdReq.valid && shiftCount <= 1.U
 
-    when (fifoInst.io.rdReq.valid || shiftCount > 0.U) {
-      fifoRdEn := false.B
-    } .elsewhen (!fifoInst.io.empty) {
-      fifoRdEn := true.B
-    }
-
     when (fifoInst.io.rdReq.valid) {
-      shiftCount := (pktWidth - 1).U
-      fifoRdEn := false.B
+      shiftCount := (payloadWidth - 1).U
       shiftEn := true.B
     } .elsewhen (shiftCount > 0.U) {
       shiftCount := shiftCount - 1.U
-      fifoRdEn := false.B
       shiftEn := true.B
-
-      if (outputValid) {
-        validOut := true.B
-      }
     } .otherwise {
-      fifoRdEn := true.B
       shiftEn := false.B
-
-      if (outputValid) {
-        validOut := false.B
-      }
     }
 
-    // Force a 1-cycle pause if not configured in continuous mode
-    /* if (continuous) {
-      fifoInst.io.rdReq.en := shiftCount <= 2.U
-    } else {
-      fifoInst.io.rdReq.en := (shiftCount <= 1.U) && !fifoInst.io.empty
-    } */
+    if (outputValid) {
+      val shiftCountDelay = RegInit(0.U(counterWidth.W))
+      shiftCount := shiftCount
+      when (shiftCount < (payloadWidth - 1 - startSeqWidth).U && 
+        shiftCountDelay >= stopSeqWidth.U) {
+
+        io.validOut := RegNext(true.B)
+      } .otherwise {
+        io.validOut := RegNext(false.B)
+      }
+    }
   }
 }
 
 
 object GenSerializer extends App {
   val pktWidth = 4
-  val continuous = true
-  val defaultVal = 1
+  val startSeq = Seq(false.B)
+  val stopSeq = Seq(true.B)
+  val defaultSOut = true
+  val outputValid = false
 
-  chisel3.Driver.execute(args, () => new Serializer(pktWidth, continuous,
-    defaultVal, 16))
+  chisel3.Driver.execute(args, () => new Serializer(pktWidth, startSeq,
+    stopSeq, defaultSOut, outputValid, 16))
 }
